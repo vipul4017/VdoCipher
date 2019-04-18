774d6b9a755272f1d4015516215accc4655c8461
vipul garg <vipul@so.city>
2019-04-18 18:52:44 +0530

    api url changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const SAVE_BROWSER_NAME = 'SAVE_BROWSER_NAME';
 export const LOGIN_USER = 'LOGIN_USER';
-export const API_URL = 'https://ttt-vipul-backend.herokuapp.com/';
+export const API_URL = 'https://ttt-vipul-backend.herokuapp.com';
